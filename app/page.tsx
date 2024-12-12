4bc5c9e9b8795b6ce30c6b02b9cd9048041d95ef
EdivandoK <133924806+EdivandoK@users.noreply.github.com>
2024-12-12 00:24:37 +0000

    primeira inclusão Edi

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Principal from "./principal"
 
 export default function Home() {
   return (
-    <div>
-      <h1>WEB UTFPR</h1>
-    </div>
+    <>
+      <h1 className="text-center">WEB UTFPR 2</h1>
+      <Principal/>
+      <h2>Rodape Edi</h2>
+    </>
   );
 }
